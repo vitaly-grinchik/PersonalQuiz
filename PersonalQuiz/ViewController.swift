41efbb9650e5fa268d0c0061aaf957c429fc69f9
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-12-25 23:05:19 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Виталий Гринчик on 25.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
