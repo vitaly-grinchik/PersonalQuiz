75860a449457c2d14394dad855643eff7c291a68
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2022-12-26 01:00:47 +0300

    Actions and Outlets definition

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
 
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangeStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet weak var rangedSlider: UISlider!
+    
     private let questions = Question.getQuestions()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+    
 }
 
